bb57a45fce4816e233bdba8cf1fa8cf1cee38e84
zero7352 <jsierra17@cps.edu>
2025-10-31 12:28:31 -0500

    Update week_11_day2.py

--- week_11_day2.py
+++ week_11_day2.py
@@ -1,4 +1,4 @@
-# open up the vscode from previous day
+e# open up the vscode from previous day
 # create a new file called: week11_day1.py
 # ----------------------------------------
 # Day 2 Review Activity: My Personal Info Generator
@@ -16,13 +16,13 @@ favorite_number = 7
 
 #  Step 2: Practice String Operations
 # 1. Print your name in uppercase
-
+print(first_name.upper())
 
 # 2. Print how many letters are in your name
-
+print(len(first_name))
 
 # 3. Combine your name and favorite color into one message
-
+print(f"My name is (first_name) and my
 
 #  Step 3: Math Practice
 # Use arithmetic operators with your favorite number
